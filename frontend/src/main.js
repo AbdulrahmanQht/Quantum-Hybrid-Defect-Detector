6ac3e6e0336827532347675215b89899139dc0d7
Abdulrahman <AbdulrahmanQht@gmail.com>
2026-02-19 17:25:58 +0300

    Added few things to fastAPI server and added routing, navbar, and cookies to frontend

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,19 +1,33 @@
 import './style.css'
 import App from './App.vue'
 import { createApp } from 'vue'
+import 'primeicons/primeicons.css'
 import Button from 'primevue/button'
+import MenuBar from 'primevue/menubar'
 import PrimeVue from 'primevue/config'
 import Aura from '@primevue/themes/aura'
+import router from './router'
+import Cookies from 'js-cookie'
 
 const app = createApp(App)
 
-// Use the new theme configuration
+const savedTheme = Cookies.get('theme')
+if (savedTheme === 'dark') {
+    document.documentElement.classList.add('p-dark')
+}
+
+app.config.globalProperties.$cookies = Cookies
+
 app.use(PrimeVue, {
     theme: {
-        preset: Aura
+        preset: Aura,
+        options: {
+            darkModeSelector: '.p-dark',
+        }
     }
 })
 
 app.component('Button', Button)
-
+app.component('MenuBar', MenuBar)
+app.use(router)
 app.mount('#app')
